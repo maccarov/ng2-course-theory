3db2bd0949146ab0af2749d4a69b4cfbc3b7b872
Макаров Сергей Владимирович <maccarov@gmail.com>
2019-03-19 21:45:51 +0300

    Раздел 2, лекция 9

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  template: `
-    <h2>Single Page</h2>
-  `,//'./car.component.html',
-  // styleUrls: ['./car.component.css']
-  styles: [`
-    h2 {
-      color: red;
-    }
-  `]
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+
 })
-export class CarComponent implements OnInit {
+export class CarComponent {
 
-  constructor() { }
+  carName = 'Ford';
+  carYear = 2017;
 
-  ngOnInit() {
+  getName () {
+    return this.carName;
   }
-
 }
